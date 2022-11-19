8454ad76e92651ddb29c8e3b593bb62534f77711
Alec Rewinkel <alecrewinkel@gmail.com>
2022-11-19 12:38:00 -0700

    code change and new routing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
     def index
+        <%= link_to 'Home', root_path %>
     end
-    
+
     def about
+        <%= link_to 'About', about_path %>
     end
    end
