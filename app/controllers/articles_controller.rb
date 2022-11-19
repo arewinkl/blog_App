208565ff7833164c81e98e7a3d50d4c65d62a103
Alec Rewinkel <alecrewinkel@gmail.com>
2022-11-19 13:46:18 -0700

    saving articles in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,15 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     def create
-        render plain: params[:article].inspect
+        #render plain: params[:article].inspect
+        @article = Article.new(article_params)
+        @article.save
        end
-       def index
-       end
+      private
+        def article_params
+         params.require(:article).permit(:title, :description)
+        end
+
+        def index
+        end
 end
