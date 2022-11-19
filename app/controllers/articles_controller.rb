cabfd41519b186aa550f0e219aedc7fcdc15e5dc
Alec Rewinkel <alecrewinkel@gmail.com>
2022-11-19 13:41:05 -0700

    controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
     def new
+        @article = Article.new
     end
+    def create
+        render plain: params[:article].inspect
+       end
+       def index
+       end
 end
